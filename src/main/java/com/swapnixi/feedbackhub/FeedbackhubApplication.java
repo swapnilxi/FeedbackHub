e98f7d13c1448c8146c2102435375a08b7f92a64
swapnil <swapnilg671@gmail.com>
2023-02-06 00:56:39 +0530

    first-commit

---
+++ src/main/java/com/swapnixi/feedbackhub/FeedbackhubApplication.java
@@ -0,0 +1,13 @@
+package com.swapnixi.feedbackhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FeedbackhubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackhubApplication.class, args);
+		System.out.println("this is feedback app");
+	}
+}
---
+++ src/test/java/com/swapnixi/feedbackhub/FeedbackhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapnixi.feedbackhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
