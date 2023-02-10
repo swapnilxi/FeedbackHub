bacdc2e7b6481860e8d34a8210af62371543d00b
Swapnil Gupta <54740310+swapnilxi@users.noreply.github.com>
2023-02-10 15:30:03 +0000

    database connected

--- src/main/java/com/swapnixi/feedbackhub/entity/User.java
+++ src/main/java/com/swapnixi/feedbackhub/entity/User.java
@@ -1,19 +1,21 @@
 package com.swapnixi.feedbackhub.entity;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
-@getters
+@Getter
+@Setter
 @Table(name= "users")
-public class Users{
+public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
