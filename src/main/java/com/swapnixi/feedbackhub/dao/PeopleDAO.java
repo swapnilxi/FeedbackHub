d15b0067337f25dd7f151a3e3ad4941a745d36d0
Swapnil Gupta <54740310+swapnilxi@users.noreply.github.com>
2023-02-20 17:53:52 +0000

    connected with postgre

---
+++ src/main/java/com/swapnixi/feedbackhub/dao/PeopleDAO.java
@@ -0,0 +1,11 @@
+package com.swapnixi.feedbackhub.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PeopleDAO {
+    // 
+
+    
+}
