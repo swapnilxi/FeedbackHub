8f3376f877449b2ccedef04f38e78880fed694e6
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-08-20 03:02:40 +0530

    Added Custom API Response Exception

--- src/main/java/com/swapnixi/feedbackhub/exception/ResourceNotFoundException.java
+++ src/main/java/com/swapnixi/feedbackhub/exception/ResourceNotFoundException.java
@@ -2,6 +2,6 @@ package com.swapnixi.feedbackhub.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String resource, String field, Object value) {
-        super(String.format("%s not found with %s : '%s'", resource, field, value));
+        super("%s not found with %s : '%s'".formatted(resource, field, value));
     }
 }
